5fc9efd7972dcc17203f8941f211597e4816b1b3
TarunZoro <tarp2605@gmail.com>
2024-06-24 23:06:09 +0530

    Enhancement

---
+++ backend/controller/taskComplete.js
@@ -0,0 +1,21 @@
+const Todo=require('../models/to-do');
+
+exports.taskComplete = async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const existingTask = await Todo.findOne({_id: taskId});
+        if(!existingTask) {
+            return res.status(400).json({message: "Task Not Found"});
+        }
+        const taskComplete = await Todo.updateOne({
+            _id: taskId
+        }, {
+            $set: {
+                done: true
+            }
+        });
+        res.status(200).json({message: "Task Updated", task: taskComplete});
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,20 @@
 const express = require('express')
-const add_route = require('./routes/add_task')
-const get_route = require('./routes/get_tasks')
-const update_route = require('./routes/task_complete')
-const delete_route = require('./routes/delete_task')
+const dbConnect = require('./config/database')
 
 const port = 3001
 const app = express()
 app.use(express.json())
 
-app.use('', add_route)
-app.use('', get_route)
-app.use('', update_route)
-app.use('', delete_route)
-
+const routes=require('./routes/route')
+app.use('/api/v1', routes)
 app.listen(port, () => {
     console.log('Server is running at port ' + port)
 })
+
+dbConnect()
+
+
+app.get("/", (req, res) => {
+    res.send(`<h1>Backend is Running and this is '/' Route</h1>`);
+  });
+  
