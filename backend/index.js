3e898468752f2dc2c074c7de82c393ef59fd3f11
Manas Pratim Das <dasmp123@gmail.com>
2024-07-08 20:25:38 +0530

    Updated port

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const dbConnect = require('./config/database')
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
